5d194e39dc27e9f1401c1a9297702971d74de8a4
Navya Sree Ram Kumar Chowdary <navichowdary1725@gmail.com>
2026-06-20 15:47:27 +0530

    june 20 2nd commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,6 @@
 from fastapi import FastAPI
 from app.api.v1.router import api_router
 
-app = FastAPI(title="Smart Attendance System")
+app = FastAPI(title="Hybrid Knowledge System Attendance")
 
 app.include_router(api_router, prefix="/api/v1")
